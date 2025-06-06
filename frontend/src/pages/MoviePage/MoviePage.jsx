60bc9780a2779620d6961483df140a17e4f4e2c3
David <dgoldrajch@gmail.com>
2025-06-06 11:26:36 +0200

    initialisation version finale

--- frontend/src/pages/MoviePage/MoviePage.jsx
+++ frontend/src/pages/MoviePage/MoviePage.jsx
@@ -1,12 +1,16 @@
 import { useParams } from 'react-router-dom';
 import { useFetchMovies } from '../Home/useFetchMoviesFromDatabase';
 import { useFetchPeople } from '../Home/useFetchPeopleFromDatabase';
+import { useFetchRating } from '../Home/useFetchRatingFromDatabase';
 import MovieDetail from '../../components/MovieDetails/MovieDetail';
+import { useLocalStorage } from '../Page_authentification/manager_id';
 
 function MoviePage() {
     const { id } = useParams();
+    const [userId, setUserId] = useLocalStorage('user_id', null);
     const { movies } = useFetchMovies();
     const { people } = useFetchPeople();
+    const { rating } = useFetchRating(userId, id);
 
     if (!movies || movies.length === 0 || !people || people.length === 0) {
         return <p>Chargement...</p>;
@@ -18,7 +22,7 @@ function MoviePage() {
         return <p>Film non trouvé.</p>;
     }
 
-    return <MovieDetail movie={movie} people={people} />;
+    return <MovieDetail movie={movie} people={people} rating={rating} />;
 }
 
 export default MoviePage;
